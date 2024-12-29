c48c3f36f2d733ab3b8056176c1912e7273145bb
yellow2041 <jeeyeon654@gmail.com>
2024-12-29 22:37:54 +0900

    useContext 학습 및 구현

--- 1-react/src/App.jsx
+++ 1-react/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Children } from "react";
 import * as MyRouter from "./lib/MyRouter";
 import * as MyLayout from "./lib/MyLayout";
 import OrderPage from "./pages/OrderPage";
@@ -9,4 +9,31 @@ import MyReact from "./lib/MyReact";
 
 const App = () => <OrderPage />;
 
-export default App;
+//export default App;
+const countContext = MyReact.createContext({});
+
+const CountProvider = ({ children }) => {
+  const [count, setCount] = React.useState(0);
+  const value = { count, setCount };
+  return (
+    <countContext.Provider value={value}>{children}</countContext.Provider>
+  );
+};
+
+const Count = () => {
+  const { count } = MyReact.useContext(countContext);
+  return <div>{count}</div>;
+};
+
+const PlusButton = () => {
+  const { count, setCount } = MyReact.useContext(countContext);
+  const handleClick = () => setCount(count + 1);
+  return <button onClick={handleClick}>PLUS</button>;
+};
+
+export default () => (
+  <CountProvider>
+    <Count />
+    <PlusButton />
+  </CountProvider>
+);
--- 1-react/src/lib/MyReact.jsx
+++ 1-react/src/lib/MyReact.jsx
@@ -2,53 +2,6 @@ import React from "react";
 import createEventEmitter from "shared/lib/EventEmitter";
 
 const MyReact = (function () {
-  function createContext(initialValue) {
-    const emitter = createEventEmitter(initialValue);
-
-    class Provider extends React.Component {
-      componentDidMount() {
-        emitter.set(this.props.value);
-      }
-
-      componentDidUpdate() {
-        emitter.set(this.props.value);
-      }
-      render() {
-        return <>{this.props.children}</>;
-      }
-    }
-
-    class Consumer extends React.Component {
-      constructor(props) {
-        super(props);
-        this.state = {
-          value: emitter.get(),
-        };
-        this.setValue = this.setValue.bind(this);
-      }
-
-      setValue(nextValue) {
-        this.setState({ value: nextValue });
-      }
-
-      componentDidMount() {
-        emitter.on(this.setValue);
-      }
-
-      componentWillUnmount() {
-        emitter.off(this.setValue);
-      }
-      render() {
-        return <>{this.props.children(this.state.value)}</>;
-      }
-    }
-
-    return {
-      Provider,
-      Consumer,
-    };
-  }
-
   const memorizedStates = [];
   let deps = [];
   const isInitialized = [];
@@ -122,8 +75,67 @@ const MyReact = (function () {
   function cleanupEffects() {
     cleanups.forEach((cleanup) => typeof cleanup === "function" && cleanup());
   }
+  function createContext(initialValue) {
+    const emitter = createEventEmitter(initialValue);
 
-  return { createContext, useState, useEffect, resetCursor, cleanupEffects };
+    function Provider({ value, children }) {
+      React.useEffect(() => {
+        emitter.set(value);
+      }, [value]);
+      return <>{children}</>;
+    }
+
+    class Consumer extends React.Component {
+      constructor(props) {
+        super(props);
+        this.state = {
+          value: emitter.get(),
+        };
+        this.setValue = this.setValue.bind(this);
+      }
+
+      setValue(nextValue) {
+        this.setState({ value: nextValue });
+      }
+
+      componentDidMount() {
+        emitter.on(this.setValue);
+      }
+
+      componentWillUnmount() {
+        emitter.off(this.setValue);
+      }
+      render() {
+        return <>{this.props.children(this.state.value)}</>;
+      }
+    }
+
+    return {
+      Provider,
+      Consumer,
+      emitter,
+    };
+  }
+  function useContext(context) {
+    const [value, setValue] = React.useState(context.emitter.get());
+
+    React.useEffect(() => {
+      context.emitter.on(setValue);
+      return () => context.emitter.off(setValue);
+    }, [context]);
+    context.emitter.on(setValue);
+
+    return value;
+  }
+
+  return {
+    createContext,
+    useState,
+    useEffect,
+    resetCursor,
+    cleanupEffects,
+    useContext,
+  };
 })();
 
 export default MyReact;
